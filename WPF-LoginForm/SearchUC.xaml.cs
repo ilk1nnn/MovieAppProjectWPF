61c7dafcb8a731943fb3e32e912099a88ca1a2b2
User <User@ASUS>
2022-10-01 00:32:55 +0400

    Add project files.

---
+++ WPF-LoginForm/SearchUC.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_LoginForm
+{
+    /// <summary>
+    /// Interaction logic for SearchUC.xaml
+    /// </summary>
+    /// 
+
+    public partial class SearchUC : UserControl,INotifyPropertyChanged
+    {
+
+        private string _poster;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public string Poster
+        {
+            get { return this._poster; }
+            set
+            {
+                if (!string.Equals(this._poster, value))
+                {
+                    this._poster = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public SearchUC()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            SendRequest request = new SendRequest();
+            if(searchedtxtb.Text.Length>1)
+            {
+                Poster = request.GetPoster(searchedtxtb.Text);
+                Title.Text = request.GetTitle(searchedtxtb.Text);
+                Genre.Content = request.GetGenre(searchedtxtb.Text);
+                Plot.Text = request.GetPlot(searchedtxtb.Text);
+            }
+        }
+    }
+}
---
+++ WPF-LoginForm/SendRequest.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_LoginForm
+{
+    public class SendRequest
+    {
+        public dynamic Data { get; set; }
+        HttpClient http = new HttpClient();
+        public string GetPoster(string source)
+        {
+            try
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+                response = http.GetAsync($@"http://www.omdbapi.com/?apikey=e4d8a8d9&s={source}&plot=full").Result;
+                var str = response.Content.ReadAsStringAsync().Result;
+                Data = JsonConvert.DeserializeObject(str);
+                string poster = Data.Search[0].Poster;
+                if (poster.Length <= 0)
+                {
+                    return $@"npa.jpg";
+                }
+                return poster;
+            }
+            catch (Exception)
+            {
+                return $@"npa.jpg";
+
+            }
+        }
+        public string GetTitle(string source)
+        {
+            try
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+                response = http.GetAsync($@"http://www.omdbapi.com/?apikey=e4d8a8d9&s={source}&plot=full").Result;
+                var str = response.Content.ReadAsStringAsync().Result;
+                Data = JsonConvert.DeserializeObject(str);
+                string title = Data.Search[0].Title;
+                return title;
+            }
+            catch (Exception)
+            {
+                return "Title";
+            }
+        }
+        public string GetGenre(string source)
+        {
+            try
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+                response = http.GetAsync($@"http://www.omdbapi.com/?apikey=e4d8a8d9&t={source}&plot=full").Result;
+                var str = response.Content.ReadAsStringAsync().Result;
+                Data = JsonConvert.DeserializeObject(str);
+                string genre = Data.Genre;
+                return genre;
+            }
+            catch (Exception)
+            {
+                return "Genre";
+            }
+        }
+        public string GetYear(string source)
+        {
+            try
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+                response = http.GetAsync($@"http://www.omdbapi.com/?apikey=e4d8a8d9&s={source}&plot=full").Result;
+                var str = response.Content.ReadAsStringAsync().Result;
+                Data = JsonConvert.DeserializeObject(str);
+                string year = Data.Search[0].Year;
+                return year;
+            }
+            catch (Exception)
+            {
+                return "Year";
+            }
+        }
+        public string GetPlot(string source)
+        {
+            try
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+                response = http.GetAsync($@"http://www.omdbapi.com/?apikey=e4d8a8d9&t={source}&plot=full").Result;
+                var str = response.Content.ReadAsStringAsync().Result;
+                Data = JsonConvert.DeserializeObject(str);
+                string plot = Data.Plot;
+                if (plot.Length <= 0)
+                {
+                    return "Not Found";
+                }
+                return plot;
+            }
+            catch (Exception)
+            {
+                return "Plot";
+            }
+        }
+    }
+}
